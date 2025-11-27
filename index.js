ca94dacc55a0f0c20dd0b6469fb2b7c3c27b5896
Behruz Jumanov <behruzjumanov55@gmail.com>
2025-11-27 13:27:07 +0500

    added new changes

---
+++ index.js
@@ -0,0 +1,67 @@
+//  alert('Welcome everyone')
+//1-chi
+
+// let L =1000;
+
+// console.log(L / 10);
+
+//2-chi
+ 
+// let M = 1200;
+// console.log(M/1000);
+
+//3-chi
+
+// let B = 2048;
+// let k = 2;
+// console.log(B/100);
+//4-chi
+// let A = 100;
+// let b = 30;
+// console.log(A/b);
+//5-chi
+
+// let x = 25;
+// console.log(String(x)[0]);
+// console.log(String(x)[1]);
+
+//6 
+
+// let a = 32;
+// let s = String(a) 
+// let one = Number(s[0]);
+// let two = Number(s[1]);
+// console.log(one + two);
+
+//7 
+// let z = 47;
+// console.log(z%10);
+// console.log(parseInt(z / 10));
+
+//8
+// let n = 234;
+// console.log(parseInt(n/100));
+
+//11
+// let  i = 981;
+// let a = parseInt(i %10);
+// let b = parseInt(i/100);
+// let c = parseInt(i %100/10);
+// console.log(a + b + c);
+
+//12
+// let  i = 981;
+
+// let a = i %10;
+// let b = parseInt(i%100/10);
+// let c = parseInt(i /100);
+
+// console.log(a*100 + b*10 + c*1);
+
+//13
+// let a = 347;
+// let x= parseInt(a/100);
+// let y = a%100;
+// console.log(y*10+x*1);
+
+//14 
